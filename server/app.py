6e69396c72555e991589a0d75142bf2158b555b6
Ochieng John Ronnie <johnronnie18@gmail.com>
2024-01-09 12:57:18 +0300

    flask application

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def bazenga():
+    return '<h1>My name is John Ronnie</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555)
